f57cab4d6790e2a22a886044a61ebaa56689d50b
mmeetty64 <57464270+mmeetty64@users.noreply.github.com>
2021-10-17 22:06:03 +0300

    Add files via upload

---
+++ php3_4.php
@@ -0,0 +1,21 @@
+<?php
+$day = 4;
+switch($day){
+    case 1: echo "Рабочий день";
+        break;
+    case 2: echo "Рабочий день";
+        break;
+    case 3: echo "Рабочий день";
+        break;
+    case 4: echo "Рабочий день";
+        break;
+    case 5: echo "Рабочий день";
+        break;
+    case 6: echo "Выходной";
+        break;
+    case 7: echo "Выходной";
+        break;
+    default: echo "Неизвестный день";
+        break;
+}
+?>
